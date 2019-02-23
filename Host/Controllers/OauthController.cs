856e100534e4481f8afc2dba0c59fb3d120a47f7
vova-lantsov-dev <vova.lantsov.25.private@gmail.com>
2019-02-23 19:32:37 +0200

    FEATURE initial oauth controller

---
+++ Host/Controllers/OauthController.cs
@@ -0,0 +1,19 @@
+using Host.Database;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Host.Controllers
+{
+    [Route("oauth")]
+    public sealed class OauthController
+    {
+        private readonly Context _context;
+        private readonly ILogger<OauthController> _logger;
+
+        public OauthController(Context context, ILogger<OauthController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+    }
+}
