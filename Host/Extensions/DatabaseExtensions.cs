85cc5c1b83d91d91b364b2428ede745fd335e3c1
vova-lantsov-dev <vova.lantsov.25.private@gmail.com>
2019-02-23 19:26:42 +0200

    FEATURE database extensions (default user)

--- Host/Extensions/DatabaseExtensions.cs
+++ Host/Extensions/DatabaseExtensions.cs
@@ -1,4 +1,7 @@
+using System.Linq;
 using Host.Database;
+using Host.Models;
+using MlkPwgen;
 
 namespace Host.Extensions
 {
@@ -6,6 +9,22 @@ namespace Host.Extensions
     {
         public static void AddDefaultData(this Context context)
         {
+            if (context.Users.Any()) return;
+            
+            context.Users.Add(new User
+            {
+                Id = 1,
+                Name = "Viacheslav Zhuravskyi",
+                Username = "lundibundi"
+            });
+            
+            var credential = new Credential
+            {
+                Id = 1,
+                Salt = PasswordGenerator.Generate(4)
+            };
+            credential.PasswordHash = PasswordExtensions.HashPassword("jerboa666", credential.Salt);
+            context.Credentials.Add(credential);
         }
     }
 }
