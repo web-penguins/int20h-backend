3c6c51cfe66704f2a3be8817d6bc98c1fcc2dcb5
vova-lantsov-dev <vova.lantsov.25.private@gmail.com>
2019-02-23 18:08:40 +0200

    FEATURE models in context

--- Host/Database/Context.cs
+++ Host/Database/Context.cs
@@ -1,3 +1,4 @@
+using Host.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace Host.Database
@@ -5,5 +6,9 @@ namespace Host.Database
     public sealed class Context : DbContext
     {
         public Context(DbContextOptions options) : base(options) { }
+        
+        public DbSet<User> Users { get; set; }
+        public DbSet<Session> Sessions { get; set; }
+        public DbSet<Credential> Credentials { get; set; }
     }
 }
