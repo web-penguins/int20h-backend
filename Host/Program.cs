492c5c3d11db2a70a580db2fe71daa0572065d33
vova-lantsov-dev <vova.lantsov.25.private@gmail.com>
2019-02-23 12:21:42 +0200

    Initial commit

---
+++ Host/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Host
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            await new WebHostBuilder()
+                .UseKestrel(options =>
+                {
+                    options.AddServerHeader = false;
+                    options.ListenLocalhost(25007);
+                })
+                .UseSockets()
+                .UseEnvironment(EnvironmentName.Development)
+                .ConfigureLogging((context, builder) =>
+                {
+                    builder.AddConsole();
+                    
+                    if (context.HostingEnvironment.IsDevelopment())
+                        builder.AddDebug();
+                })
+                .UseStartup<Startup>()
+                .UseShutdownTimeout(TimeSpan.FromSeconds(10d))
+                .SuppressStatusMessages(false)
+                .CaptureStartupErrors(true)
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .Build().RunAsync();
+        }
+    }
+}
---
+++ Host/Startup.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Host
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvcCore().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseMvc();
+        }
+    }
+}
