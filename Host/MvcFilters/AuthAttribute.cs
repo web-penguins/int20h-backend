23dab908350dc1e26eb7acef4dbbf250d75e6890
vova-lantsov-dev <vova.lantsov.25.private@gmail.com>
2019-02-23 21:30:45 +0200

    REFACTORING folder name Authorization => MvcFilters; adjust namespaces

--- Host/MvcFilters/AuthAttribute.cs
+++ Host/MvcFilters/AuthAttribute.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.Extensions.Logging;
 
-namespace Host.Authorization
+namespace Host.MvcFilters
 {
     public sealed class AuthAttribute : Attribute, IAuthorizationFilter
     {
