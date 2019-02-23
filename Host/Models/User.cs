a7c9ae7a0933685f0ecf2bc184d4a2782202078c
vova-lantsov-dev <vova.lantsov.25.private@gmail.com>
2019-02-23 19:01:52 +0200

    FIX single name field + username field

--- Host/Models/User.cs
+++ Host/Models/User.cs
@@ -4,8 +4,8 @@ namespace Host.Models
     {
         public int Id { get; set; }
         
-        public string FirstName { get; set; }
+        public string Name { get; set; }
         
-        public string LastName { get; set; }
+        public string Username { get; set; }
     }
 }
