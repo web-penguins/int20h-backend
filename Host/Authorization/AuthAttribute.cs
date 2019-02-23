4cdbbfb1fe4272d1bb0c397d7a2607b0dfe4fa73
vova-lantsov-dev <vova.lantsov.25.private@gmail.com>
2019-02-23 18:55:49 +0200

    FEATURE authorization custom attribute

---
+++ Host/Authorization/AuthAttribute.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Linq;
+using Host.Database;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Logging;
+
+namespace Host.Authorization
+{
+    public sealed class AuthAttribute : Attribute, IActionFilter
+    {
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (!(context.HttpContext.RequestServices
+                .GetService(typeof(ILogger<AuthAttribute>)) is ILogger<AuthAttribute> logger))
+            {
+                context.Result = new StatusCodeResult(500);
+                return;
+            }
+
+            if (!context.HttpContext.Request.Headers.TryGetValue("Token", out var token))
+            {
+                logger.LogDebug("Unauthorized received");
+                context.Result = new UnauthorizedResult();
+                return;
+            }
+
+            if (!(context.HttpContext.RequestServices.GetService(typeof(Context)) is Context dbContext))
+            {
+                logger.LogError("dbContext is null");
+                context.Result = new StatusCodeResult(500);
+                return;
+            }
+
+            var session = dbContext.Sessions.FirstOrDefault(s => s.Token == token);
+            if (session == null)
+            {
+                logger.LogDebug("Access denied");
+                context.Result = new StatusCodeResult(403);
+            }
+            else logger.LogDebug("Authorized successfully, user id: {0}", session.UserId);
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+        }
+    }
+}
